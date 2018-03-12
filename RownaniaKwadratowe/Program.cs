f4b61e7650770fd42e2837a02265b956b7ac6a8d
unknown <student@IBLAB732P-04.ib.polsl.pl>
2018-03-12 09:09:47 +0100

    sprawdzenie czy delta jest dodatnia

--- RownaniaKwadratowe/Program.cs
+++ RownaniaKwadratowe/Program.cs
@@ -17,11 +17,22 @@ namespace RownaniaKwadratowe
 
             double delta = b * b - 4 * a * c;
 
-            double x1 = (-b + Math.Sqrt(delta)) / 2 * a;
-            double x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+            
 
-            Console.WriteLine("Pierwiastki to: {0} i {1}", x1,x2);
-            Console.ReadKey();
+            if(delta>=0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / 2 * a;
+                double x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+
+                Console.WriteLine("Pierwiastki to: {0} i {1}", x1, x2);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Brak rozwiazan w dziedzinie R");
+            }
+
+          
         }
     }
 }
